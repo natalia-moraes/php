425467d92620d84f788714bee2eb70958136f613
natalia-moraes <52804317+natalia-moraes@users.noreply.github.com>
2019-07-16 14:03:33 -0300

    Update exemplo01.php

--- exemplo01.php
+++ exemplo01.php
@@ -1,9 +1,30 @@
 <?php
 
 $nome = "Natalia";
+$DataNascimento = "1987";
 
 // mostrar tipo da variavel
-
 var_dump($nome);
+
+//limpar variavel, destruir 
+$nome1= "Joao";
+echo $nome1;
+echo "<br/>";
+//caso tenha mais de uma variavel você pode utilizar , para acrescentar as demais no mesmo unset
+unset($nome1);
+echo($nome1);
+
+//tratar variavel para exibir somente se existir
+$nome1 = "Joao";
+if(isset($nome1)){
+  echo $nome1;
+}
+
+//concatenação
+$nome1= "Joao"
+$nome2 = "Silva";
+$NomeCompleto = $nome1 . " " . $nome2;
+
+echo $NomeCompleto;
 
 ?>

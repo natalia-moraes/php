3ab21e4591a5ff7804f194008349edd4d2685651
natalia-moraes <52804317+natalia-moraes@users.noreply.github.com>
2019-07-16 20:57:07 -0300

    Operadores de Atribuição

---
+++ exemplo06.php
@@ -0,0 +1,21 @@
+<?php
+  
+  // Operador de atribuição =
+    $nome = "Usuario";
+  
+  // Operador de concatenação .
+    echo $nome . "mais alguma coisa<br>";
+  
+ // Operador composto (vai juntar as informações "Usuario em treinamentos"
+    $nome .= "Em treinamento";
+    echo $nome;
+    
+ // Operador de atribuição com valores
+    $valortotal = 0;
+    $valortotal +=100;
+    $valortotal +=25;
+    $valortotal -=10;
+    $valortotal *= .9; //90% do valor
+      echo $valortotal;
+      
+?>

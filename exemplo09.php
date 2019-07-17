65161228253afe426154a22f7d727af09f4ca679
natalia-moraes <52804317+natalia-moraes@users.noreply.github.com>
2019-07-17 13:46:44 -0300

    Operadores spaceship

---
+++ exemplo09.php
@@ -0,0 +1,15 @@
+<?php
+  
+  $a = 50;
+  $b = 35;
+  
+  //comparação do tipo maior, menor ou igual
+  var_dump($a <=> $b);
+  
+  /*
+  maior > mostra 1
+  menor < mostra -1
+  igual = mostra 0 
+  */
+  
+?>

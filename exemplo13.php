491fe200ab2e18d456101f5b3ae23523c1debfaf
natalia-moraes <52804317+natalia-moraes@users.noreply.github.com>
2019-07-17 20:54:03 -0300

    Diferença aspas duplas e simples

---
+++ exemplo13.php
@@ -0,0 +1,13 @@
+<?php
+  $nome = "Natalia";
+  $nome2 = 'Caroline';
+  
+  var_dump($nome, $nome2);
+
+  // faz uma verificação dessa variavel primeiro e exibe na tela "ABC Natalia"
+  echo "ABC $nome";
+  
+  // entende que tudo é texto e exibe na tela da mesma forma "ABC $nome"
+  echo 'ABC $nome';
+
+?>

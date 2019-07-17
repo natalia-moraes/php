f1efac68a216d767baf87bd0b410cd4caea6a9d9
natalia-moraes <52804317+natalia-moraes@users.noreply.github.com>
2019-07-16 21:03:11 -0300

    Operadores Aritméticos

---
+++ exemplo07.php
@@ -0,0 +1,22 @@
+<?php
+
+  $a = 10;
+  $b = 2;
+    echo $a + $b; //adição
+    echo "<br>";
+    
+    echo $a - $b; //subtração
+    echo "<br>";
+    
+    echo $a * $b; //multiplicação
+    echo "<br>";
+    
+    echo $a / $b; //divisão
+    echo "<br>";
+    
+    echo $a % $b; //modulo
+    echo "<br>";
+    
+    echo $a ** $b; //elevado ao quadrado
+    echo "<br>";
+?>

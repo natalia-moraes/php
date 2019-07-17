e1e57525dd75db3321af3834ec48e44fb02f3d66
natalia-moraes <52804317+natalia-moraes@users.noreply.github.com>
2019-07-16 21:10:29 -0300

    Operadores

---
+++ exemplo08.php
@@ -0,0 +1,16 @@
+<?php
+
+  $a = 30;
+  $b = 55;
+  
+  var_dump($a > $b); //retorna se realmente a é maior que b;
+  echo "<br>";
+  var_dump($a < $b);
+  echo "<br>";
+  
+  //Operador de identidade = comparação para valor
+  var_dump($a == $b); 
+  
+  //Operador de = comparação para valor e tipo
+  var_dump($a === $b); 
+?>

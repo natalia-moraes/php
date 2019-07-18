666d7a826e8b5f44254643129047d929dc519bf3
natalia-moraes <52804317+natalia-moraes@users.noreply.github.com>
2019-07-18 20:48:15 -0300

    Funções: string maiscula, minuscula, primeira letra

---
+++ exemplo14.php
@@ -0,0 +1,25 @@
+<?php
+
+  $nome = "joAo raNgEl";
+  
+  //função letra MAIUSCULA
+  echo strtoupper($nome);
+  
+  echo "<br>";
+  
+  //função letra minuscula
+  echo strtolower($nome);
+  
+   echo "<br>";
+  
+  //função para mudar 1 letra de cada palavra
+  echo ucwords($nome);
+  
+   echo "<br>";
+  
+  //função para mudar 1 letra - geralmente aplica se em paragrafo
+  echo ucfirst($nome);
+  
+  
+  
+?>

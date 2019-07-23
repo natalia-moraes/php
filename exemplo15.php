66863ea14146d9b5b1acb97e5ada1a9d9fe3c593
natalia-moraes <52804317+natalia-moraes@users.noreply.github.com>
2019-07-23 20:10:03 -0300

    Update exemplo15.php

--- exemplo15.php
+++ exemplo15.php
@@ -7,11 +7,12 @@
   $q = strpos($frase, $palavra);
    var_dump($q);
   
-  //
+  //função para encontrar a partir de um indice
   $texto = substr($frase, 0, $q);
   var_dump($texto);
  
   $texto2 = substr($frase, $q + strlen($palavra), strlen($frase));
+  echo "<br>";
   var_dump($texto2);
   
 ?>
